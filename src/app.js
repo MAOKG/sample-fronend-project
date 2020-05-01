58bb03dcd5653648646748729ee29a8e6663c5d6
Maosen <maosen1993@gmail.com>
2020-04-30 20:25:48 -0400

    add css loader

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDom from "react-dom";
+import styles from "./app.css";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className={styles.red}>Hello World</h1>
     </div>
   );
 };
